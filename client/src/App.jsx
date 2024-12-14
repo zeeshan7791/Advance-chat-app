f8d26771235f96c291251b0f09db19b5c6c2d6c4
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-12-14 07:12:54 +0100

    Routes wrapped in Protected Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,29 @@
 // eslint-disable-next-line no-unused-vars
 import React,{lazy} from 'react'
 import {BrowserRouter,Routes,Route} from "react-router-dom"
+import ProtectedRoute from './components/auth/ProtectedRoute'
+import NotFound from './pages/NotFound'
 const Home=lazy(()=>import('./pages/Home'))
 const Login=lazy(()=>import('./pages/Login'))
 const Chat=lazy(()=>import('./pages/Chat'))
 const Groups=lazy(()=>import('./pages/Groups'))
 const App = () => {
+  let user=true
   return (
 <BrowserRouter>
 <Routes>
 
+  <Route element={<ProtectedRoute user={user}/>}>
+
   <Route path="/" element={<Home/>}/>
-  <Route path="/login" element={<Login/>}/>
-  <Route path="/chat/:chatId" element={<Chat/>}/>
-  <Route path="/groups" element={<Groups/>}/>
+    <Route path="/chat/:chatId" element={<Chat/>}/>
+    <Route path="/groups" element={<Groups/>}/>
+</Route>
+
+  <Route path="/login" 
+  element={<ProtectedRoute user={!user} redirect='/'><Login/> </ProtectedRoute>}/>
+ <Route path="*" element={<NotFound/>}/>
+ 
 </Routes>
 </BrowserRouter>
   )
