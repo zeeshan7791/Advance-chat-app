1fcdba6856b2a53da94873e0370363c7d0e7b8ae
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-12-14 07:14:42 +0100

    home page Wrap layout created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useContext } from 'react'
+import AppLayout from '../components/layout/AppLayout'
 
 
 const Home = () => {
@@ -15,4 +16,5 @@ const Home = () => {
   )
 }
 
-export default Home
+// eslint-disable-next-line react-refresh/only-export-components
+export default AppLayout()(Home)
