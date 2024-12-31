ddc96f8111c0c6aef90de8ec74c50dc272b81d93
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-12-31 15:13:21 +0500

    Style Custom Link Component

--- client/src/components/styled/StyledComponents.jsx
+++ client/src/components/styled/StyledComponents.jsx
@@ -1,7 +1,7 @@
 
 import { styled } from '@mui/material'
 
-
+import {Link as LinkComponent}from "react-router-dom"
 export const VisuallyHiddenInput =styled ("input") ({
 border:"0",
 clip:"rect(0,0,0,0)",
@@ -15,3 +15,11 @@ width:"1"
 
 })
 
+export const Link=styled(LinkComponent)`({
+    text-decoration:none,
+    color:black,
+    padding:1rem,
+    &:hover{
+        background-color:#f0f0f0;
+    }
+})`
