6af3e2e97bc8a8621c124bef2301d367466c3851
Zeeshan Yousaf <zeeshanento34@gmail.com>
2025-01-03 11:56:20 +0500

    complete design of search Dialog

--- client/src/components/specific/SearchDialog.jsx
+++ client/src/components/specific/SearchDialog.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
-
+import {
+  Dialog,
+  DialogTitle,
+  InputAdornment,
+  List,
+  Stack,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
+import { useInputValidation } from "6pp";
+import { Search as SearchIcon } from "@mui/icons-material";
+import UserItem from "../shared/UserItem";
 const SearchDialog = () => {
-  return (
-    <div>
-      Search
-    </div>
-  )
-}
+  const search = useInputValidation("");
+  let isLoadingSendFriendRequest=false
+  const addFriendHandler=()=>{
+    console.log("id")
+  }
+  const sampleUsers = [
 
-export default SearchDialog
+    {
+      name: "zeeshan",
+      _id: 1,
+      avatar: "avatar.png",
+    },
+    {
+      name: "Ali",
+      _id: 2,
+      avatar: "avatar.png",
+    },
+    {
+      name: "Aqeel",
+      _id: 3,
+      avatar: "avatar.png",
+    },
+  ];
+  const [users,SetUsers]=useState(sampleUsers)
+  return (
+    <>
+      <Dialog open>
+        <Stack padding={"2rem"} direction={"column"} width={"25rem"}>
+          <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+          <TextField
+            label=""
+            value={search.value}
+            onChange={search.changeHandler}
+            variant="outlined"
+            size="small"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <List>
+            {users.map((user, index) => (
+              <UserItem
+                key={index}
+                user={user}
+                handler={addFriendHandler}
+                handlerIsLoading={isLoadingSendFriendRequest}
+              />
+            ))}
+          </List>
+        </Stack>
+      </Dialog>
+    </>
+  );
+};
+
+export default SearchDialog;
